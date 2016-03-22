ef24a3fec5c832aa7c7da450e11da62b8139d3bb
Daniel McBryde <mcbryded@gmail.com>
2016-03-22 17:39:42 -0500

    pass first tests

--- src/login-form.js
+++ src/login-form.js
@@ -1,2 +1,24 @@
+export default class LoginForm {
+  constructor(x, email) {
+    this.userDirectory = [
+      { username: `aaron@theironyard.com`, password: `password123` },
+      { username: `admin@google.com`, password: `pandas` },
+      { username: email, password: `honeycrisp` },
+    ];
+  }
 
-export default undefined;
+  validate(username, password) {
+    this.username = username;
+    this.password = password;
+
+    const findValid = (snowball, current) => {
+      if (current.username === username && current.password === password) {
+        return true;
+      }
+
+      return snowball;
+    };
+
+    return this.userDirectory.reduce(findValid, false);
+  }
+}
---
+++ src/userDirectory.js
@@ -0,0 +1,5 @@
+export default userDirectory [
+  aaron@theironyard.com: password123,
+  admin@google.com: pandas,
+  email@email.com: honeycrisp
+];
